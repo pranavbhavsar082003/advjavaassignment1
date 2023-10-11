8bb076968c8d0314a8a8b7440dd1130996034309
Pranav Bhavsar <pranav0806bhavsar@gmail.com>
2023-10-11 18:01:57 -0400

    module info and pom.xml

--- src/main/java/com/example/advjavaassignment1/DBUtility.java
+++ src/main/java/com/example/advjavaassignment1/DBUtility.java
@@ -1,5 +1,7 @@
 package com.example.advjavaassignment1;
 
 public class DBUtility {
-
+    private static String User = "Pranav200545944";
+    private static String password  = "tK7pwBPi0L";
+    private static String connectionURL = "jdbc:mysql//172.31.22.43:3306/Pranav200545944";
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.advjavaassignment1 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.advjavaassignment1 to javafx.fxml;
     exports com.example.advjavaassignment1;
